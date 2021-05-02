e28b3be813c973439b6fa7f11532e4d55fe5d23f
Riko Bgrff <Riko.bagirli777@gmail.com>
2021-05-02 16:56:03 +0400

    Add project files.

---
+++ LastProjectWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LastProjectWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+/*Margin="145,164,515,174"
+Margin = "486,164,174,174"
+  Margin="320,286,340,52">*/
